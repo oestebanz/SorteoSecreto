0af085bb49b40310d4ec1799a059d2cec1067629
Esteban Garcia <estebangarcia0613@gmail.com>
2026-05-02 16:04:34 -0500

    feat(model): implement executeDraw with random retry algorithm and restriction validation

--- src/model/Raffle.java
+++ src/model/Raffle.java
@@ -2,6 +2,8 @@ package model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
 
 /**
  * This class represents the raffle itself, including its participants,
@@ -151,36 +153,82 @@ public class Raffle {
 
     }
 
-    public void annul(){
+    public void annul() {
         this.status = RaffleStatus.ANNULLED;
     }
 
-    public Raffle cloneRaffle( String newName){
+    public Raffle cloneRaffle(String newName) {
         Raffle clone = new Raffle(newName, this.description, this.suggestedBudget, this.eventDate);
-         for (int i = 0; i < participants.size(); i++){
-          Participant  current = participants.get(i);
-         
-          clone.addParticipant(current.getEmail(),current.getName());
-         }
-         return clone;
+        for (int i = 0; i < participants.size(); i++) {
+            Participant current = participants.get(i);
+
+            clone.addParticipant(current.getEmail(), current.getName());
+        }
+        return clone;
     }
-    
 
-    public String getStatusReport(){
+    public String getStatusReport() {
 
-        String report = "Name: "+ getName();
-        report += "\nDescription: "+ getDescription();
-        report += "\nSuggested Budget: "+ getSuggestedBudget();
-        report += "\nEvent Date: "+ getEventDate();
-        report += "\nStatus: "+ getStatus();
-        
-        for(int i = 0; i < participants.size(); i++){
+        String report = "Name: " + getName();
+        report += "\nDescription: " + getDescription();
+        report += "\nSuggested Budget: " + getSuggestedBudget();
+        report += "\nEvent Date: " + getEventDate();
+        report += "\nStatus: " + getStatus();
+
+        for (int i = 0; i < participants.size(); i++) {
             report += "\n- Name: " + participants.get(i).getName();
             report += "\n- Email: " + participants.get(i).getEmail();
-            
 
         }
         return report;
     }
-        
+
+    public boolean executeDraw() {
+        if (this.status != RaffleStatus.CREATED) {
+            return false;
+        }
+        if (participants.size() < 2) {
+            return false;
+        }
+
+        Random random = new Random();
+
+        for (int j = 0; j < 1000; j++) {
+            ArrayList<Participant> shuffled = new ArrayList<Participant>(participants);
+            Collections.shuffle(shuffled, random);
+            ArrayList<Assignment> tempAssignments = new ArrayList<Assignment>();
+
+            for (int i = 0; i < shuffled.size(); i++) {
+
+                Participant giver = shuffled.get(i);
+                Participant receiver = shuffled.get((i + 1) % shuffled.size());
+                Assignment newAssignment = new Assignment(giver, receiver);
+                tempAssignments.add(newAssignment);
+
+            }
+
+            if (isValidAssignmentList(tempAssignments)) {
+                this.assignments = tempAssignments;
+                this.status = RaffleStatus.DRAWN;
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidAssignmentList(ArrayList<Assignment> assignmentList) {
+        for (int i = 0; i < assignmentList.size(); i++){
+            Assignment currentAssignment = assignmentList.get(i);
+
+            for(int j = 0; j < restrictions.size(); j++){
+                Restriction currentRestriction = restrictions.get(j);
+                if (currentAssignment.getGiver().equals(currentRestriction.getFrom()) && currentAssignment.getReceiver().equals(currentRestriction.getTo())){
+                    return false;
+                }
+            }
+        }
+        return true;
+}
+
 }
